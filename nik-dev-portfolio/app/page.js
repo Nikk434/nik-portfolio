2a864aa143406cdb4ea93447e90847850964f2d8
Nikk434 <nskshirsagar434@gmail.com>
2025-12-10 10:12:27 +0530

    Added contact section

--- nik-dev-portfolio/app/page.js
+++ nik-dev-portfolio/app/page.js
@@ -2,6 +2,7 @@ import Hero from "@/components/hero";
 import Skills from "@/components/skills";
 import Experience from "@/components/experience";
 import Projects from "@/components/projects";
+import Contact from "@/components/contacts";
 export default function Home() {
   return( 
     <>
@@ -9,6 +10,7 @@ export default function Home() {
       <Skills/>
       <Experience/>
       <Projects/>
+      <Contact/>
     </>
   )
 }
